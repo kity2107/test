a8e1acc31be2d2138c59277a2a793581aa6eb50e
Cristian Panontini <kity2107@gmail.com>
2022-06-07 20:25:26 -0300

    ejercicis con mocks/fake/spy

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -8,13 +8,18 @@ const fakeBooks = [
   },
 ];
 
+const mockSpyGetAll = jest.fn();
+
 const MongoLibStub = {
-  getAll: () => [...fakeBooks],
+  getAll: mockSpyGetAll,
   create: () => {},
 };
 
 jest.mock('../lib/mongo.lib.js', () =>
-  jest.fn().mockImplementation(() => MongoLibStub)
+  jest.fn().mockImplementation(() => ({
+    getAll: mockSpyGetAll,
+    create: () => {},
+  }))
 );
 
 describe('Test para nuestro booksService', () => {
@@ -27,15 +32,20 @@ describe('Test para nuestro booksService', () => {
 
   describe('test para getBooks', () => {
     test('Deberia retornar una lista de libros', async () => {
+      mockSpyGetAll.mockResolvedValue(fakeBooks);
       const books = await service.getBooks({});
       console.log(books);
       expect(books.length).toEqual(1);
+      expect(mockSpyGetAll).toHaveBeenCalled(); //  ---->>le pregunto si fue llamada la funcion espia
+      expect(mockSpyGetAll).toHaveBeenCalledTimes(1); // ---->>le pregunto cuantas veces fue llamado
+      expect(mockSpyGetAll).toHaveBeenCalledWith('books', {}); // -->> con q argumentos fue llamado
     });
 
     test('Deberia retornar un libro con el nombre harry poter', async () => {
+      mockSpyGetAll.mockResolvedValue([{ _id: 1, name: 'Harry Poter2' }]);
       const books = await service.getBooks({});
       console.log(books);
-      expect(books[0].name).toEqual('Harry Poter');
+      expect(books[0].name).toEqual('Harry Poter2');
     });
   });
 });
